a505b6ec4e6b10168e1aa12f4404ac74179e0800
Paul Pessoa <paulmspessoa@gmail.com>
2021-10-26 23:33:59 -0300

    primeira etapa de selscao de aributos

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const fetchPokemon = () => {
-    const url = 'https:pokeapi.co/api/v2/pokemon/1';
-    fetch(url)
-        .then( res => {
-            return res.json();
-        })
-        .then( data => {
-            console.log(data)
-        })
-};
+    const promises = [] ;   
+    for( let i = 1; i <= 150; i++ ) {
+    const url = `https:pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', '),
+            id: data.id
+        }));
+        console.log(pokemon);
+    });
+};   
+
 
 fetchPokemon();
+
+
+
+
+
