d81ad8341566b5191876733da76d8b1a42351ba2
Paul Pessoa <paulmspessoa@gmail.com>
2021-10-27 22:26:42 -0300

    estilizando cards

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const podekex = document.getElementById("pokedex");
+console.log("pokedex");
+
 const fetchPokemon = () => {
     const promises = [] ;   
-    for( let i = 1; i <= 150; i++ ) {
+    for( let i = 1; i <= 50; i++ ) {
     const url = `https:pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
     }
@@ -13,12 +16,28 @@ const fetchPokemon = () => {
             type: data.types.map((type) => type.type.name).join(', '),
             id: data.id
         }));
-        console.log(pokemon);
+        displayPokemon(pokemon);
     });
 };   
 
+const displayPokemon = (pokemon) => {
+    
+    const pokemonHTMLString = pokemon.map((pokeman) => `
+    <li class="card">
+        <img class="card"-image src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        <p class="card-subtitle">Type: ${pokeman.type}</p>
+    </li>
+ 
+    `   
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
 
 fetchPokemon();
+
+
 
 
 
