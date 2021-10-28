ce779c9a6f53a850bf3f8be2c7962931c9ec2a0b
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2021-10-27 23:14:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-const podekex = document.getElementById("pokedex");
-console.log("pokedex");
+const pokedex = document.getElementById('pokedex');
 
 const fetchPokemon = () => {
-    const promises = [] ;   
-    for( let i = 1; i <= 100; i++ ) {
-    const url = `https:pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
     }
-
     Promise.all(promises).then((results) => {
-        const pokemon = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            type: data.types.map((type) => type.type.name).join(', '),
-            id: data.id
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
         }));
         displayPokemon(pokemon);
     });
-};   
+};
 
 const displayPokemon = (pokemon) => {
-    
-    const pokemonHTMLString = pokemon.map((pokeman) => `
-    <li class="card">
-        <img class="card-image" src="${pokeman.image}"/>
-        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-        <p class="card-subtitle">Type: ${pokeman.type}</p>
-    </li>
- 
-    `   
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
         )
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
-    console.log(pokemon);
 };
-
 
 fetchPokemon();
